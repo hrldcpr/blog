a2c86f9e9d7569b1ccf98374f14fb296c28b7c37
Harold Cooper <h@x.st>
2022-03-16 14:55:13 -0400

    slicker but seemingly slower :'(

--- assets/latex3d.js
+++ assets/latex3d.js
@@ -1,11 +1,9 @@
 const OMEGA = -0.05; // turns per second
-const spinners = Array.from(document.getElementsByClassName('latex3d'));
 
+const {style} = document.documentElement;
 const spin = (t) => {
   const theta = (OMEGA * t / 1000) % 1;
-  spinners.forEach(spinner => {
-    spinner.style.transform = `rotateX(0.1turn) rotateY(${theta}turn)`;
-  });
+  style.setProperty('--theta', `${theta}turn`);
   requestAnimationFrame(spin);
 };
 requestAnimationFrame(spin);

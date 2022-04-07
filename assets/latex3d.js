16113ddbe7d412b83f88e4826b8f5a9c797e7bb7
Harold Cooper <h@x.st>
2022-04-06 21:12:56 -0400

    let css transitions do (almost all of) the work instead of js. cpu no longer pegged, though still high

--- assets/latex3d.js
+++ assets/latex3d.js
@@ -1,12 +1,13 @@
 const OMEGA = -0.05; // turns per second
 
 const latex3ds = Array.from(document.getElementsByClassName('latex3d'));
-const spin = (t) => {
-  const theta = (OMEGA * t / 1000) % 1;
+const spin = () => {
+  const t = performance.now();
+  const theta = (OMEGA * t / 1000) + 0.5;
   latex3ds.forEach(latex3d => {
     latex3d.style.setProperty('--theta', `${theta}turn`);
     latex3d.style.setProperty('--untheta', `${-theta}turn`);
   });
-  requestAnimationFrame(spin);
+  setTimeout(spin, 10*1000);
 };
-requestAnimationFrame(spin);
+spin();

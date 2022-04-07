60727638b20ee4ac0e594da87afcf4e3a7de166c
Harold Cooper <h@x.st>
2022-04-06 20:28:09 -0400

    never use setProperty on a :root css variable! it re-styles the entire document. much faster now, though still pegs cpu :"(

--- assets/latex3d.js
+++ assets/latex3d.js
@@ -1,10 +1,12 @@
 const OMEGA = -0.05; // turns per second
 
-const {style} = document.documentElement;
+const latex3ds = Array.from(document.getElementsByClassName('latex3d'));
 const spin = (t) => {
   const theta = (OMEGA * t / 1000) % 1;
-  style.setProperty('--theta', `${theta}turn`);
-  style.setProperty('--untheta', `${-theta}turn`);
+  latex3ds.forEach(latex3d => {
+    latex3d.style.setProperty('--theta', `${theta}turn`);
+    latex3d.style.setProperty('--untheta', `${-theta}turn`);
+  });
   requestAnimationFrame(spin);
 };
 requestAnimationFrame(spin);
